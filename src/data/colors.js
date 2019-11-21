be999512de271dc9dac6ded7d85d27d917a7d62b
Dirt Deodara <thecrowleaps@gmail.com>
2019-11-21 14:02:23 -0800

    dummy data created

---
+++ src/data/colors.js
@@ -0,0 +1,15 @@
+export default colors = [
+  "Yellow/White",
+  "Grey/Black",
+  "Grey",
+  "Grey/White",
+  "White",
+  "White/Yellow",
+  "White/Orange",
+  "White/Green",
+  "Blue",
+  "Purple",
+  "Brown",
+  "Black"
+];
+
