b438621297d2336a9002be221eb806d90a74ca39
Dirt Deodara <thecrowleaps@gmail.com>
2020-01-24 22:17:33 -0800

    created some custom  hooks

--- src/hooks/student.js
+++ src/hooks/student.js
@@ -1,23 +1,37 @@
 import { useState, useEffect } from 'react'
 import { fetchStudent } from '../services/studentsApi';
+import { schoolIpAddr, homeIpAddr, beziIpAddr} from '../data/ipAddresses.js';
 
 export const useStudent = id => {
   const [student, setStudent] = useState();
   const [loading, setLoading] = useState(true);
 
   const promoteStripe = () => {
-    // TODO use service to call route
-    // e.g. fetch('http://${schoolIpAddr}:3000/api/v1/students/:id/stripe, { })
+    return fetch(`http://${beziIpAddr}:3000/api/v1/students/${student.id}/stripe`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        coach_who_promoted: 'Chris' //TODO change this to chris and then to eddie
+      })
+    })
   };
 
   const promoteBelt = () => {
-    // TODO use service to call route
-    // e.g. fetch('http://${schoolIpAddr}:3000/api/v1/students/:id/belt, { })
+    return fetch(`http://${beziIpAddr}:3000/api/v1/students/${student.id}/belt`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        coach_who_promoted: 'dirt'
+      })
+    })
   }
 
   useEffect(() => {
-    fetchStudent(id, setStudent)
-      .then(() => setLoading(false));
+    fetchStudent(id, setStudent, setLoading)
   }, [id]);
 
   return { student, loading, promoteStripe, promoteBelt };
