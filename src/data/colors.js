76c188382ae9cd3b3033991b6c2ea89aa0a8cc74
Dirt Deodara <thecrowleaps@gmail.com>
2020-01-26 17:31:45 -0800

    edited the colors.js file to reflect the changed to the data on the backend

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,15 +1,15 @@
 export default colors = [
-  "Yellow/White/Youth",
-  "Grey/Black/Youth",
-  "Grey/Grey/Youth",
-  "Grey/White/Youth",
-  "White/Yellow",
-  "White/Orange",
-  "White/Green",
-  "White",
-  "Blue",
-  "Purple",
-  "Brown",
-  "Black"
+  "Yellow-White-Yellow",
+  "Grey-Black-Grey",
+  "Grey-Grey-Grey",
+  "Grey-White-Grey",
+  "White-Yellow-White",
+  "White-Orange-White",
+  "White-Green-White",
+  "White-White-White",
+  "Blue-Blue-Blue",
+  "Purple-Purple-Purple",
+  "Brown-Brown-Brown",
+  "Black-Black-Black"
 ];
 
