81193efb376b3f5e06bc3b53f290f4dab2354c36
Dirt Deodara <thecrowleaps@gmail.com>
2020-02-06 15:51:32 -0800

    brought all of my icons into one file

---
+++ src/utils/icons.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { AntDesign, Ionicons, MaterialIcons, FontAwesome  } from "@expo/vector-icons";
+
+export const backIcon = <AntDesign name="doubleleft" size={50} style={{ right: 2 }} />;
+export const addIcon = <Ionicons name="md-add" size={65} />;
+export const closeIcon = <AntDesign name="close" size={45} style={{ paddingLeft: 100 }} />;
+export const addStudentIcon = <MaterialIcons name="person-add" size={50} style={{ right: 2 }} />;
+export const searchIcon = <FontAwesome name="search" size={30} />;
+export const burgerIcon = <FontAwesome name="bars" size={50} />;
