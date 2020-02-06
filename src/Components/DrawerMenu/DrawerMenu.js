c48e2e15a1c4f10c2df192f53e782706839ed7b7
Dirt Deodara <thecrowleaps@gmail.com>
2020-02-06 15:51:10 -0800

    now my drawerMenu is animated!!!

--- src/Components/DrawerMenu/DrawerMenu.js
+++ src/Components/DrawerMenu/DrawerMenu.js
@@ -1,59 +1,70 @@
-import React from "react";
-import { withRouter } from "react-router-native";
-import { View, Text, TouchableOpacity } from "react-native";
-import PropTypes from "prop-types";
-import styles from "./drawerMenuStyles";
-import { AntDesign } from "@expo/vector-icons";
-import colors from "../../data/colors";
-import colorChecker from "../../utils/colorChecker";
-import colorExtractor from "../../utils/colorExtractor";
-import BeltIndicator from "../BeltImage/BeltIndicator";
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-native';
+import { Animated, Text, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './drawerMenuStyles';
+import colors from '../../data/colors';
+import BeltIndicator from '../BeltImage/BeltColorIcon';
+import { closeIcon } from '../../utils/icons';
 
 const DrawerMenu = ({ handleTouch, position, history }) => {
-  const listOfLinks = colors.map((color, i) => {
-    const [mainColor, secondaryColor] = colorExtractor(color);
+  const [openAnim] = useState(new Animated.Value(280));
+  useEffect(() => {
+    Animated.timing(
+      openAnim,
+      {
+        toValue: position.isOpen ? 85 : 280,
+        duration: 150,
+      }
+    ).start();
+  }, [position.isOpen]);
+  
 
+
+
+  const listOfLinks = colors.map((color, i) => {
     return (
       <TouchableOpacity
         key={i}
         onPress={() => {
           history.push(`/studentList/${color}`);
         }}
-        style={{ flexDirection: "column", borderWidth: 2 }}
+        style={{ flexDirection: 'column', borderWidth: 2 }}
       >
         <BeltIndicator
-          beltwidth={180}
-          beltheight={10}
-          flexDirection={"column"}
-          mainColor={mainColor}
-          secondaryColor={secondaryColor}
+          beltWidth={180}
+          beltHeight={10}
+          flexDirection={'column'}
+          color={color}
         />
       </TouchableOpacity>
     );
   });
-
+  
   return (
-    <TouchableOpacity
+    <Animated.View
       onPress={() => handleTouch()}
       style={{
         ...styles.container,
         borderWidth: 3,
         transform: [
           {
-            translateX: position.isOpen ? 85 : 280
+            translateX: openAnim
           }
         ]
       }}
     >
-      <AntDesign name="close" size={45} style={{ paddingLeft: 110 }} />
-      <TouchableOpacity onPress={() => history.push("/newStudentForm")}>
+      <TouchableOpacity onPress={() => handleTouch()}>
+        {closeIcon}
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => history.push('/newStudentForm')}>
         <Text style={styles.link}>New Student Form</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => history.push("/")}>
+      <TouchableOpacity onPress={() => history.push('/')}>
         <Text style={styles.link}>All Students</Text>
       </TouchableOpacity>
       {listOfLinks}
-    </TouchableOpacity>
+    </Animated.View>
   );
 };
 
