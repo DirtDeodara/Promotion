9cea98d3af9dac679eb2b9d58170ec45bc0df7ac
Dirt Deodara <thecrowleaps@gmail.com>
2020-02-09 13:29:47 -0800

    can now search in Header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,65 @@
-import React from 'react';
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from 'react-router-native';
+import { View, TouchableOpacity, TextInput } from "react-native";
+import styles from "./headerStyles";
+import { searchIcon, burgerIcon } from '../../utils/icons';
 
-function Header() {
+const Header = ({ handleTouch, history }) => {
+  const [searchValue, setSearchValue] = useState();
+
+  const handleSubmit = () => {
+    history.push(`/studentList/name/${searchValue}`);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Promotion</Text>
+      <TextInput name="searchInput"
+        placeholder="  Search by student name"
+        onChangeText={setSearchValue}
+        value={searchValue}
+        style={{
+          borderColor: "black",
+          borderWidth: 2,
+          width: 200,
+          height: 40,
+          top: 2,
+          borderRadius: 15,
+          bottom: 30,
+          backgroundColor: "#E2E0DA",
+          textAlign: 'center'
+        }}
+      />
+
+      <TouchableOpacity name='search-button'
+        onPress={handleSubmit}
+        style={{
+          height: 40,
+          width: 45,
+          borderWidth: 2,
+          borderRadius: 15,
+          backgroundColor: "#E2E0DA",
+          top: 2,
+          paddingLeft: 6,
+          paddingTop: 2,
+          right: 25
+        }}
+      >
+        {searchIcon}
+      </TouchableOpacity>
+
+      <TouchableOpacity name='burgerMenuButton'
+        style={{ bottom: 3, right: 1 }}
+        onPress={() => handleTouch()}
+      >
+        {burgerIcon}
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-  },
-  text: {
-    fontSize: 30
-    
-  }
-})
+Header.propTypes = {
+  handleTouch: PropTypes.func.isRequired
+};
 
-export default Header;
+export default withRouter(Header);
---
+++ src/Components/Header/headerStyles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+  container: {
+    width: 340,
+    height: 45,
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10
+  },
+  text: {
+    fontSize: 50,
+  }
+})
