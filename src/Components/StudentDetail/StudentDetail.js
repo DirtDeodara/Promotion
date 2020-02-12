2acc541a00211616e9de1228291913bf062db8c4
Dirt Deodara <thecrowleaps@gmail.com>
2020-02-12 11:27:16 -0800

    brought DELETE functionality into StudentDetail comp

--- src/Components/StudentDetail/StudentDetail.js
+++ src/Components/StudentDetail/StudentDetail.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Alert } from 'react-native';
 import { withRouter } from 'react-router-native';
 import PropTypes from 'prop-types';
 import NavButton from '../../Components/NavButton/NavButton';
@@ -7,15 +7,15 @@ import BeltImage from '../BeltImage/BeltImage';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import PromotionButtons from '../PromotionButtons/PromotionButtons';
 import styles from './studentDetailStyles';
-import { backIcon, addIcon } from '../../utils/icons';
+import { backIcon, addIcon, deleteIcon, editIcon } from '../../utils/icons';
+import { useStudent } from '../../hooks/student';
+import { ipAddrToUse } from '../../data/ipAddresses';
 const moment = require("moment");
 const placeHolderImage = require("../../../assets/a.jpg"); //TODO this is just a placeholer!!
-import { useStudent } from '../../hooks/student';
 
-const StudentDetail = ({ match }) => {
+const StudentDetail = ({ match, history }) => {
   const { student, loading, promoteStripe, promoteBelt } = useStudent(match.params.id);
   const [promotionType, setPromotionType] = useState(null);
-  // console.log(student) //TODO why it this being called so many times?
 
   if(loading) return (
     <View> 
@@ -36,10 +36,52 @@ const StudentDetail = ({ match }) => {
     if(promotionType === 'belt') return promoteBelt();
   }
 
+  const deleteAlertHandler = () => {
+    Alert.alert(
+      //title
+      'Warning!',
+      //body
+      'Are you sure you want to delete this student?',
+      [
+        {text: 'Yes', onPress: handleDelete},
+        {text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel'},
+      ],
+      { cancelable: false }
+      //clicking out side of alert will not cancel
+    );
+  }
+
+  const handleDelete = () => {
+    return fetch(`http://${ipAddrToUse}:3000/api/v1/students/${student.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(history.goBack())
+  }
+
+  const handlePressEdit = () => {
+    history.push(`/editForm/${student.id}`);
+   
+  }
+
   return (
     <View>
       <View style={{ ...styles.container, backgroundColor: "white" }}>
         <View style={styles.container}>
+          <TouchableOpacity 
+            name='deleteButton'
+            onPress={deleteAlertHandler}
+            style={{ position: 'absolute', top: 10, left: 10 }}>
+            {deleteIcon}
+          </TouchableOpacity>
+          <TouchableOpacity 
+            name='editButton'
+            onPress={handlePressEdit}
+            style={{ position: 'absolute', top: 10, left: 295 }}>
+            {editIcon}
+          </TouchableOpacity>
           <Text name='student-name' style={styles.nameText}>{student.name}</Text>
           <Text name='student-age'style={styles.text}>{age} years old</Text>
           <Image name='student-image'style={styles.image} source={placeHolderImage} />
