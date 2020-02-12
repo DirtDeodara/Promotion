1dc28e15764080f9bab4ac5b51d2e2eb96332e09
Dirt Deodara <thecrowleaps@gmail.com>
2020-02-12 11:24:25 -0800

    added some icons to icons.js to export to whereever they are needed

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { AntDesign, Ionicons, MaterialIcons, FontAwesome  } from "@expo/vector-icons";
+import { AntDesign, Ionicons, MaterialIcons, FontAwesome  } from '@expo/vector-icons';
 
-export const backIcon = <AntDesign name="doubleleft" size={50} style={{ right: 2 }} />;
-export const addIcon = <Ionicons name="md-add" size={65} />;
-export const closeIcon = <AntDesign name="close" size={45} style={{ paddingLeft: 100 }} />;
-export const addStudentIcon = <MaterialIcons name="person-add" size={50} style={{ right: 2 }} />;
-export const searchIcon = <FontAwesome name="search" size={30} />;
-export const burgerIcon = <FontAwesome name="bars" size={50} />;
+export const backIcon = <AntDesign name='doubleleft' size={50} style={{ right: 2 }} />;
+export const addIcon = <Ionicons name='md-add' size={65} />;
+export const closeIcon = <AntDesign name='close' size={45} style={{ paddingLeft: 100 }} />;
+export const addStudentIcon = <MaterialIcons name='person-add' size={50} style={{ right: 2 }} />;
+export const searchIcon = <FontAwesome name='search' size={30} />;
+export const burgerIcon = <FontAwesome name='bars' size={50} />;
+export const deleteIcon = <AntDesign name='deleteuser' size={30} />;
+export const editIcon = <MaterialIcons name='edit' size={30} />;
+export const addStripeIcon = <Ionicons name='md-add' size={30} />;
+
