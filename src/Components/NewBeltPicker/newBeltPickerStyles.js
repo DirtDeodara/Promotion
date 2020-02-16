bce0f233111295c04b9e847170fdb643821004dc
Dirt Deodara <thecrowleaps@gmail.com>
2020-02-15 19:39:54 -0800

    styles for NewBeltPikcer

---
+++ src/Components/NewBeltPicker/newBeltPickerStyles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    width: 200,
+    height: 100,
+    backgroundColor: '#E2E0DA',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    borderColor: 'black',
+    borderWidth: 5,
+    borderRadius: 10,
+  },
+  beltIndicator: {
+    borderWidth: 3,
+    borderRadius: 5,
+    bottom: 6,
+    height: 30,
+    width: 60,
+  }
+});
