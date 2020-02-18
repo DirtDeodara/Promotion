26a885a9f9b842d19f9f40f8a93bca753d778389
Dirt Deodara <thecrowleaps@gmail.com>
2020-02-18 08:06:35 -0800

    cleaned up the code in App

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { NativeRouter, Route } from "react-router-native";
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { NativeRouter, Route } from 'react-router-native';
 import StudentList from './src/Components/StudentList/StudentList';
 import StudentDetail from './src/Components/StudentDetail/StudentDetail';
 import NewStudentForm from './src/Components/NewStudentForm/NewStudentForm';
-import Header from "./src/Components/Header/Header";
-import DrawerMenu from "./src/Components/DrawerMenu/DrawerMenu";
+import Header from './src/Components/Header/Header';
+import DrawerMenu from './src/Components/DrawerMenu/DrawerMenu';
+import EditForm from './src/Components/EditForm/EditForm';
 
 export default function App() {
   const [position, setPosition] = useState({ isOpen: false });
@@ -18,11 +19,12 @@ export default function App() {
     <View style={styles.container}>
       <NativeRouter>
         <Header handleTouch={handleTouch} />
-        <Route path="/newStudentform" component={NewStudentForm} />
-        <Route path="/studentDetail/:id" component={StudentDetail} />
-        <Route path="/studentList/color/:color" component={StudentList} />
-        <Route path="/studentList/name/:name" component={StudentList} />
-        <Route exact path="/" component={StudentList} />
+        <Route path='/newStudentform' component={NewStudentForm} />
+        <Route path='/editForm/:id' component={EditForm} />
+        <Route path='/studentDetail/:id' component={StudentDetail} />
+        <Route path='/studentList/color/:color' component={StudentList} />
+        <Route path='/studentList/name/:name' component={StudentList} />
+        <Route exact path='/' component={StudentList} />
         <DrawerMenu handleTouch={handleTouch} position={position}/>
       </NativeRouter>
     </View>
@@ -32,12 +34,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D4AF37',
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
-  },
-  burger: {
-    right: 20
+    backgroundColor: '#EF476F',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
   }
 });
