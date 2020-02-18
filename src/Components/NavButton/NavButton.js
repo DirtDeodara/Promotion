3882417bbdef2c3425a33e119bf039a78ec2a0cd
Dirt Deodara <thecrowleaps@gmail.com>
2020-02-18 08:10:02 -0800

    using a try/catch in NavButton to handle an error case

--- src/Components/NavButton/NavButton.js
+++ src/Components/NavButton/NavButton.js
@@ -2,18 +2,22 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import { useHistory } from "react-router-native";
 import PropTypes from 'prop-types';
-// import { AntDesign } from "@expo/vector-icons";
 import styles from './navButtonStyles';
 
-function NavButton({ handleSubmit, icon, hasSubmitted }) { 
+function NavButton({ icon, handleSubmit, hasSubmitted }) { 
   const history = useHistory();
  
   return (
       <TouchableOpacity
         disabled={hasSubmitted}
         onPress={ async() => {
-          handleSubmit && await handleSubmit();
-          history.goBack();
+          try{
+            handleSubmit && await handleSubmit();
+            history.goBack();
+          }
+          catch(e) {
+            console.log('did nothing')
+          }
         }}
         style={styles.container}
       >
