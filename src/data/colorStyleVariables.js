1da84ccc5076a28fd80eb71ed242678f0897cd60
Dirt Deodara <thecrowleaps@gmail.com>
2020-03-12 15:43:03 -0700

    color styles variables

---
+++ src/data/colorStyleVariables.js
@@ -0,0 +1,33 @@
+const goldTheme = {
+  mainBackgroundColor: '#D39000',
+  borderColor: '#281F00',
+  containerBackgroundColor: '#F2EAC9',
+  loadingContainerColor: '#000',
+  drawerMenuBackground: '#D6D6B1',
+  navButtonColor: '#2B5B8A',
+  searchColor: '#E2E0DA'
+}
+
+const neonPinkTheme = {
+  mainBackgroundColor: '#FF2B5C',
+  borderColor: '#011936',
+  containerBackgroundColor: '#BFE3FC',
+  loadingContainerColor: '#000',
+  drawerMenuBackground: '#DDA8FF',
+  navButtonColor: '#2B5B8A',
+  searchColor: '#E2E0DA'
+}
+
+const lightTheme = {
+  mainBackgroundColor: '#E6E8FA',
+  borderColor: '#0A0A0F',
+  containerBackgroundColor: '#F5F5FA', // TODO maybe change back to #FFF
+  loadingContainerColor: '#000',
+  drawerMenuBackground: '#D4D5D9',
+  navButtonColor: '#4997d0',
+  searchColor: '#FFF'
+}
+
+const colorTheme = lightTheme;
+
+export default colorTheme;
