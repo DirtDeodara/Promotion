41210e0f0abe87470536d5d3975a50fc3f352372
Dirt Deodara <thecrowleaps@gmail.com>
2020-03-12 15:42:36 -0700

    icons

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -3,7 +3,7 @@ import { AntDesign, Ionicons, MaterialIcons, MaterialCommunityIcons, FontAwesome
 
 export const backIcon = <AntDesign name='doubleleft' size={50} style={{ right: 2 }} />;
 export const addIcon = <Ionicons name='md-add' size={65} />;
-export const closeIcon = <AntDesign name='close' size={45} style={{ paddingLeft: 100 }} />;
+export const closeIcon = <FontAwesome name='close' size={50} style={{ bottom: 3, right: 3, paddingLeft: 4}} />;
 export const addStudentIcon = <MaterialIcons name='person-add' size={50} style={{ right: 2 }} />;
 export const searchIcon = <FontAwesome name='search' size={30} />;
 export const burgerIcon = <FontAwesome name='bars' size={50} />;
